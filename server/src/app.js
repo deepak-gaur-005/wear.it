f34165c36141d730e40df3f96563cdb9e37da979
Deepak Gaur <deepakguar111@gmail.com>
2026-09-06 15:53:49 +0530

    feat: update package description and improve CORS origin handling

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,9 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGINS.split(",") || "http://localhost:5173",
+        origin: process.env.CORS_ORIGINS
+            ? process.env.CORS_ORIGINS.split(",")
+            : "http://localhost:5173",
         credentials: true
     })
 );
